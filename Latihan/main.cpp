90bf1c1b2e38abd396e86e6016e6da8d0b0137a5
Irsyad Jamal Pratama Putra <irsyad.putra@aprisma.co.id>
2018-11-17 10:52:36 +0700

    Initial Commit

---
+++ Latihan/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Latihan
+//
+//  Created by Irsyad Jamal Pratama Putra on 17/11/18.
+//  Copyright © 2018 Irsyad Jamal Pratama Putra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
