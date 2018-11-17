74ed6d90934d10b75c201f0d0e1c7831b307718a
Irsyad Jamal Pratama Putra <irsyad.putra@aprisma.co.id>
2018-11-17 11:22:55 +0700

    Initial commit

---
+++ Latihan/user.cpp
@@ -0,0 +1,9 @@
+//
+//  User.cpp
+//  Latihan
+//
+//  Created by Irsyad Jamal Pratama Putra on 17/11/18.
+//  Copyright © 2018 Irsyad Jamal Pratama Putra. All rights reserved.
+//
+
+#include "User.hpp"
---
+++ Latihan/user.hpp
@@ -0,0 +1,14 @@
+//
+//  User.hpp
+//  Latihan
+//
+//  Created by Irsyad Jamal Pratama Putra on 17/11/18.
+//  Copyright © 2018 Irsyad Jamal Pratama Putra. All rights reserved.
+//
+
+#ifndef User_hpp
+#define User_hpp
+
+#include <stdio.h>
+
+#endif /* User_hpp */
